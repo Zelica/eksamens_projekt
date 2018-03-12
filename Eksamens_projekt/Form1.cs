e1bb9937f5f31196c2f260a7024618bec9374269
Lene Borup Randløv <lenerandlov@gmail.com>
2018-03-12 10:04:34 +0100

    lol

--- Eksamens_projekt/Form1.cs
+++ Eksamens_projekt/Form1.cs
@@ -26,7 +26,33 @@ namespace Eksamens_projekt
             InitializeComponent();
         }
 
-        private void restart()
+        private void Highscore_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a placeholder for a future feature");
+        }
+
+        private void Settings_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a placeholder for a future feature");
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ClientSize = new Size(896, 640);
+
+            restart();
+
+            player_picture.Parent = background;
+            mob_picture.Parent = background;
+            Start.Parent = background;
+            Highscore.Parent = background;
+            Settings.Parent = background;
+        }
+
+        public void restart()
         {
             background.Image = Eksamens_projekt.Properties.Resources.start_screen;
             player_picture.Image = null;
@@ -41,17 +67,38 @@ namespace Eksamens_projekt
             Player_turn = true;
         }
 
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            background.Image = Eksamens_projekt.Properties.Resources.Background1;
+
+            Player1 = new Player();
+            player_picture.Image = Eksamens_projekt.Properties.Resources.character;
+
+            Mob = new Slime();
+            mob_picture.Image = Eksamens_projekt.Properties.Resources.Basic_Slime;
+
+            Player_Life.Text = $"HP: {Player1.Current_Life} / {Player1.Max_Life}";
+            Mob_Life.Text = $"HP: {Mob.Current_Life} / {Mob.Max_Life}";
+
+            Attack.Visible = true;
+            Player_Life.Visible = true;
+            Mob_Life.Visible = true;
+
+            floor++;
+        }
+
         private void new_floor()
         {
             floor++;
 
-            level = Convert.ToInt32(Math.Floor(floor/ 5f));
-            switch(level)
+            level = Convert.ToInt32(Math.Floor(floor / 10f));
+            switch (level)
             {
                 default:
                     Random r1 = new Random();
                     Slime_type = r1.Next(1, 6);
-                    switch(Slime_type)
+                    switch (Slime_type)
                     {
                         case 1:
                             Mob = new Fire();
@@ -81,69 +128,42 @@ namespace Eksamens_projekt
             Player1.Current_Life = Player1.Max_Life;
             Player_Life.Text = $"HP: {Player1.Current_Life} / {Player1.Max_Life}";
             Mob_Life.Text = $"HP: {Mob.Current_Life} / {Mob.Max_Life}";
-        }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            this.ClientSize = new Size(896, 640);
-
-            restart();
-            player_picture.Parent = background;
-            mob_picture.Parent = background;
-            Start.Parent = background;
-            Highscore.Parent = background;
-            Settings.Parent = background;
-        }
-
-        private void Start_Click(object sender, EventArgs e)
-        {
-            background.Image = Eksamens_projekt.Properties.Resources.Background1;
-
-            Player1 = new Player();
-            player_picture.Image = Eksamens_projekt.Properties.Resources.character;
-            Mob = new Slime();
-            mob_picture.Image = Eksamens_projekt.Properties.Resources.Basic_Slime;
-
-            Player_Life.Text = $"HP: {Player1.Current_Life} / {Player1.Max_Life}";
-            Mob_Life.Text = $"HP: {Mob.Current_Life} / {Mob.Max_Life}";
-
-            Attack.Visible = true;
-            Player_Life.Visible = true;
-            Mob_Life.Visible = true;
-
-            floor++;
-        }
-
-        private void Highscore_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("This is a placeholder for a future feature");
-        }
-
-        private void Settings_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("This is a placeholder for a future feature");
+            Player_turn = true;
         }
 
         private void Attack_Click(object sender, EventArgs e)
         {
-            Mob.Current_Life = Mob.Current_Life - Player1.Damage;
-            Mob_Life.Text = $"HP: {Mob.Current_Life} / {Mob.Max_Life}";
-
-            if (Mob.Current_Life > 1)
+            if (Player_turn == true)
             {
-                Player1.Current_Life = Player1.Current_Life - Mob.Damage;
-                Player_Life.Text = $"HP: {Player1.Current_Life} / {Player1.Max_Life}";
-                if (Player1.Current_Life < 1)
+                Mob.Current_Life = Mob.Current_Life - Player1.Damage;
+                Mob_Life.Text = $"HP: {Mob.Current_Life} / {Mob.Max_Life}";
+                Player_turn = false;
+
+                if (Mob.Current_Life > 1)
                 {
-                    MessageBox.Show("You Failed");
-                    restart();
+                    Mob_turn();
+                }
+                else
+                {
+                    score = score + Mob.score;
+                    new_floor();
                 }
             }
-            else
+        }
+
+        public void Mob_turn()
+        {
+            Player1.Current_Life = Player1.Current_Life - Mob.Damage;
+            Player_Life.Text = $"HP: {Player1.Current_Life} / {Player1.Max_Life}";
+
+            if (Player1.Current_Life < 1)
             {
-                score = score + Mob.score; 
-                new_floor();
+                MessageBox.Show("You Failed");
+                restart();
             }
+
+            Player_turn = true;
         }
     }
 }
